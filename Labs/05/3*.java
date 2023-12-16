85ff7e5a374b1f5cdec7cf2dc9ddc9aad88da368
Jana Angeloska <132594803+janaangeloska@users.noreply.github.com>
2023-12-16 19:10:57 +0100

    3*.java

---
+++ Labs/05/3*.java
@@ -0,0 +1,18 @@
+/*Дадена е една двојно поврзана листa и со N јазли кои во себе содржат по еден природен број. Треба да се сортира листата со помош на сортирањето со меурчиња (bubble sort). 
+Во првиот ред од влезот е даден бројот на јазли во листата, а потоа во вториот ред се дадени јазлите од кои е составена. На излез треба да се испечатат јазлите на сортираната листа.
+Име на класата: BubbleSortDLL
+Забелешка: При реализација на задачата МОРА да се користи дадената структура, а не да користат помошни структури како низи и сл.
+-----------------
+
+Given is a doubly linked list with N nodes that each contain a natural number. The list should be sorted using bubble sort.
+In the first line of the input, the number of nodes in the list is given, and then in the second line, the nodes of which it is composed are given. The output should print the nodes of the sorted list
+Class Name: BubbleSortDLL
+Note: When performing the task, the given structure MUST be used, and not auxiliary structures such as arrays, etc.*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BubbleSortDLL {
+	
+}
