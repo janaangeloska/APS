974870b3c726584a57a54d7c27bbd735eb118dd2
Jana Angeloska <132594803+janaangeloska@users.noreply.github.com>
2023-12-19 21:24:12 +0100

    1.java

---
+++ Labs/09/1.java
@@ -0,0 +1,262 @@
+/*Со помош на бинарно дрво потребно е да проверите дали при preorder изминување на дрвото важи дека секој следен елемент има вредност за 2 поголема од претходниот (треба да се испечати true или false). Притоа доколку ви е потребно можете да користите дополнителни функции, рекурзија и дополнителни структури.
+/
+Using a binary tree and preorder traversal check if every next element is equal to the previous element + 2. You are allowed to use additional functions, recursion, and additional structures.*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Stack;
+
+class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
+
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+class SLL<E> {
+    public SLLNode<E> first;
+
+    public SLL() {
+        this.first = null;
+    }
+    @Override
+    public String toString() {
+        String ret = new String();
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            ret += tmp;
+            while (tmp.succ != null) {
+                tmp = tmp.succ;
+                ret += " " + tmp;
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+    public void insertFirst(E o) {
+        SLLNode<E> ins = new SLLNode<E>(o, null);
+        ins.succ = first;
+        first = ins;
+    }
+
+    public void insertLast(E o) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while (tmp.succ != null)
+                tmp = tmp.succ;
+            tmp.succ = new SLLNode<E>(o, null);
+        } else {
+            insertFirst(o);
+        }
+    }
+}
+
+class BNode<E> {
+    
+    public E info;
+    public BNode<E> left;
+    public BNode<E> right;
+    
+    static int LEFT = 1;
+    static int RIGHT = 2;
+    
+    public BNode(E info) {
+        this.info = info;
+        left = null;
+        right = null;
+    }
+    
+    public BNode(E info, BNode<E> left, BNode<E> right) {
+        this.info = info;
+        this.left = left;
+        this.right = right;
+    }
+    
+}
+
+class BTree<E> {
+    
+    public BNode<E> root;
+    
+    public BTree() {
+        root = null;
+    }
+    
+    public BTree(E info) {
+        root = new BNode<E>(info);
+    }
+    
+    public void makeRoot(E elem) {
+        root = new BNode<E>(elem);
+    }
+    
+    public BNode<E> addChild(BNode<E> node, int where, E elem) {
+        
+        BNode<E> tmp = new BNode<E>(elem);
+        
+        if (where == BNode.LEFT) {
+            if (node.left != null)  // veke postoi element
+                return null;
+            node.left = tmp;
+        } else {
+            if (node.right != null) // veke postoi element
+                return null;
+            node.right = tmp;
+        }
+        
+        return tmp;
+    }
+    
+    public void inorder() {
+        System.out.print("INORDER: ");
+        inorderR(root);
+        System.out.println();
+    }
+    
+    public void inorderR(BNode<E> n) {
+        if (n != null) {
+            inorderR(n.left);
+            System.out.print(n.info.toString()+" ");
+            inorderR(n.right);
+        }
+    }
+
+    public void preorder() {
+        System.out.print("PREORDER: ");
+        preorderR(root);
+        System.out.println();
+    }
+    
+    public void preorderR(BNode<E> n) {
+        if (n != null) {
+            System.out.print(n.info.toString()+" ");
+            preorderR(n.left);
+            preorderR(n.right);
+        }
+    }
+    
+    public void postorder() {
+        System.out.print("POSTORDER: ");
+        postorderR(root);
+        System.out.println();
+    }
+    
+    public void postorderR(BNode<E> n) {
+        if (n != null) {
+            postorderR(n.left);
+            postorderR(n.right);
+            System.out.print(n.info.toString()+" ");
+        }
+    }
+    
+    public void inorderNonRecursive() {
+        Stack<BNode<E>> s = new Stack<BNode<E>>();
+        BNode<E> p = root;
+        System.out.print("INORDER (nonrecursive): ");
+        
+        while (true) {
+            // pridvizuvanje do kraj vo leva nasoka pri sto site koreni
+            // na potstebla se dodavaat vo magacin za podocnezna obrabotka
+            while (p != null) {
+                s.push(p);
+                p = p.left;
+            }
+            
+            // ako magacinot e prazen znaci deka stebloto e celosno izminato
+            if (s.isEmpty())
+                break;
+
+            p = s.peek();
+            // pecatenje (obrabotka) na jazelot na vrvot od magacinot
+            System.out.print(p.info.toString()+" ");
+            // brisenje na obraboteniot jazel od magacinot
+            s.pop();
+            // pridvizuvanje vo desno od obraboteniot jazel i povtoruvanje na
+            // postapkata za desnoto potsteblo na jazelot
+            p = p.right;
+            
+        }
+        System.out.println();
+        
+    }    
+     
+}
+
+public class ConsecutiveNumbers {
+    private static void preorderTraversal(BNode<Integer> root, SLL<Integer> list) {
+        Stack<BNode<Integer>> stack = new Stack<>();
+        BNode<Integer> curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+            if (curr != null) {
+                list.insertFirst(curr.info);
+                stack.push(curr.right);
+                curr = curr.left;
+            } else {
+                curr = stack.pop();
+            }
+        }
+    }
+    private static boolean preorderCheck(SLL<Integer> list) {
+        SLLNode<Integer> current = list.first;
+
+        while (current.succ != null) {
+            if (Math.abs(current.element - current.succ.element) != 2) {
+                return false;
+            }
+            current = current.succ;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int i,j,k;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        @SuppressWarnings("unchecked")
+        BNode<Integer> nodes[] = new BNode[N];
+        BTree<Integer> tree = new BTree<Integer>();
+
+        for (i=0;i<N;i++)
+            nodes[i] = null;
+
+        for (i = 0; i < N; i++) {
+            String line = br.readLine();
+            st = new StringTokenizer(line);
+            int index = Integer.parseInt(st.nextToken());
+            nodes[index] = new BNode<Integer>(Integer.parseInt(st.nextToken()));
+            String action = st.nextToken();
+            if (action.equals("LEFT")) {
+                BNode<Integer> child = tree.addChild(nodes[Integer.parseInt(st.nextToken())], BNode.LEFT, nodes[index].info);
+                nodes[index] = child;
+            } else if (action.equals("RIGHT")) {
+                BNode<Integer> child = tree.addChild(nodes[Integer.parseInt(st.nextToken())], BNode.RIGHT, nodes[index].info);
+                nodes[index] = child;
+            } else {
+                // this node is the root
+                tree.makeRoot(nodes[index].info);
+                nodes[index] = tree.root;
+            }
+        }
+
+        br.close();
+        // vasiot kod ovde
+        SLL<Integer> preorderList = new SLL<>();
+        preorderTraversal(nodes[0], preorderList);
+        boolean result = preorderCheck(preorderList);
+        System.out.println(result);
+    }
+}
