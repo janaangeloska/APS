3c79a4c55e9b14c682ac683899990132771334bd
Jana Angeloska <132594803+janaangeloska@users.noreply.github.com>
2024-01-07 00:45:55 +0100

    2*.java

---
+++ Labs/09/2*.java
@@ -0,0 +1,29 @@
+/*На еден натпревар има учесници кои освоиле одреден број на поени. Потребно е да одредите на кое место завршил натпреварувачот со даден број на поени. Во првата редица е даден бројот на натпреварувачи N, а во следните N редици се дадени поените на секој од натпреварувачите. На кај е даден бројот на поени M за натпреварувачот за кој треба да одредите на кое место завршил.
+На излез треба да се испечати ранг-местото на натпреварувачот кој освоил M поени. Оваа задача треба да ја решите без користење на низи, листи или методи за сортирање.
+Влез: Во првата редица е даден бројот на натпреварувачи N, а во следните N редици се дадени поените на секој од натпреварувачите. На кај е даден бројот на поени M за натпреварувачот за кој треба да одредите на кое место завршил.
+Излез: Ранг-местото на натпреварувачот кој освоил M поени.
+Пример: 
+5
+2
+3
+5
+4
+1
+4
+Излез: 2
+/
+At a competition, there are competitors who score a certain number of points. You should determine the rank of the competitor who scored a given number of points. In the first line, you're given the number of competitors N, аnd in the next N lines you're given the points that each competitor scored. In the last line, you're given the score M of the competitor whose rank you should determine.
+You should print the rank of the competitor who scored M points. You should solve this problem without using any arrays, lists or sorting methods.
+Input: In the first line you're given the number of competitors N, аnd in the next N lines you're given the points that each competitor scored. In the last line, you're given the score M of the competitor whose rank you should determine.
+Output: The rank of the competitor who scored M points.
+Example: 
+5
+2
+3
+5
+4
+1
+4
+Output: 2*/
+
+//TODO
