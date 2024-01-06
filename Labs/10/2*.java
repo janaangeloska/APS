9341074198c93dee84baaf45f37d1f2f580bce4e
Jana Angeloska <132594803+janaangeloska@users.noreply.github.com>
2024-01-07 00:47:08 +0100

    2*.java

---
+++ Labs/10/2*.java
@@ -0,0 +1,7 @@
+/*Нека е даден совршен лавиринт (во форма како на аудиториските вежби - како влез од карактери). Ваша задача е да изгенерирате граф со матрица на соседство (ненасочен и нетежински) од дадениот влез и да ја испечатите патеката од почетното (темето означено со S) до крајното теме (темето означено со Е).
+Патеката ја печатите на следниот начин: ги печатите координатите на јазлите кои ги изминувате (т.е ги печатите позициите од влезот)
+/
+It is given a perfect maze (in the same form as presented on the aud. exercises - input of characters). Your task is to create a graph (unoriented and unweighted) by using the adjacency matrix from the given input and to print the path from the initial vertex (vertex denoted by S) to the final vertex (vertex denoted by E).
+Print the path as follows: print the coordinates of the unfolded vertices (i.e. print the positions from the input).*/
+
+//TODO
