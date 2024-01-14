b8885f4ce8d46cb1c0f46e0c68b7cb73c01c7f3a
Jana Angeloska <132594803+janaangeloska@users.noreply.github.com>
2024-01-14 02:06:11 +0100

    1.java

---
+++ Exams/II part/1.java
@@ -0,0 +1,3 @@
+/*Дадено ви е бинарно пребарувачко дрво. Напишете рекурзивна функција inorderSuccessor која што за дадена вредност во дрвото ќе го најде нејзиниот следбеник.
+/
+Given a binary search tree, you need to write a recursive function inorderSuccessor that will return the successor of a given value.*/
